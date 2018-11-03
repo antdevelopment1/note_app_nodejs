13559e1b2a9e66d1fd985b74f6d6a63e45b9f0cc
April Copes <antdevelopment1@gmail.com>
2018-11-02 23:32:59 -0400

    Example of fs and os node modules

--- note_app/index.js
+++ note_app/index.js
@@ -1,1 +1,14 @@
-console.log('Initial Setup. This is a test');
+console.log('Starting App');
+
+// Built in node modules
+const fs = require('fs');
+const os = require('os');
+
+let user = os.userInfo();
+console.log(user);
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err) {
+    if (err) {
+        console.log('error');
+    }
+});
