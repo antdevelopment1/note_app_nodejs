59bd14d252b8dd15efc5a97c725098ba9406d28c
April Copes <antdevelopment1@gmail.com>
2018-11-04 16:33:29 -0500

    Refactors addNote to make our fetchNotes reusable

--- note_app/notes.js
+++ note_app/notes.js
@@ -4,33 +4,30 @@ console.log('Starting notes.js');
 // Imports node module
 const fs = require('fs');
 
+let fetchNotes = () => {
+    try {
+        let notesString = fs.readFileSync('notes-data.json');
+        // We then parse the data into
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
+
+let saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 // Function expression with an anonymous function that accepts two arguments title and body
 let addNote = (title, body) => {
-
-    // Saves an empty array inside our anonymous function
-    let notes = [];
-
+   
+    let notes = fetchNotes();
     // Saves an obejct inside our annoymous function
     let note = {
         title: title,
         body: body
     };
-
-    // We add the try catch for error handeling. If the file does not exsist yet then it will move on to the line where we push the note
-    // into our notes object. The next step would be to run fs.writeFileSync which creates the notes-data.json file and we pass the notes
-    // object to the file and it gets converted to json prior to being written to the file. 
-    // If the file already exsists then we go into our try/catch statement and read our current file notes-data.json
-    // The current json string insdie that file gets assigned to noteString. From there we parse it back into a javascript object
-    // And then we push that object into our notes array and proceed to fs.writeFileSync which will add our whole array with the new note 
-    // added and converted back into json
-    try {
-        let notesString = fs.readFileSync('notes-data.json');
-        // We then parse the data into
-        notes = JSON.parse(notesString);
-    } catch (e) {
-
-    }
-
+    
     // Will store an array with all notes that already exsists inside the notes array that have the title of the note the user will try to create
     // This line will return a bollean value to see if the note that got passed to our 
     let duplicateNotes = notes.filter((note) => note.title === title);
@@ -38,9 +35,13 @@ let addNote = (title, body) => {
     // If the length of our duplicateNotes === 0 meaning we have not yet duplicated the title name then we want to go ahead an execute our if statements code block
     // Otherwise if we have duplicated the title name then we just won't push that data into our array
     if (duplicateNotes.length === 0) {
+        // Pushes the the strignified note into our notes array
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-    }
+        // Calls the saveNote function on our notes object and writes it to our json file and converts the contents int the file back to json
+        saveNotes(notes);
+        // We return the original object back from this function containing our title and body
+        return note;
+    } 
 
 };
 
@@ -62,3 +63,5 @@ module.exports = {
     readNote: readNote,
     removeNote: removeNote
 };
+
+
