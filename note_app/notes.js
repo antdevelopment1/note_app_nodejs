fc0764346d08890425b716a0246dfb0f56632fd8
April Copes <antdevelopment1@gmail.com>
2018-11-03 22:14:52 -0400

    creates and exports readNote and removeNote function

--- note_app/notes.js
+++ note_app/notes.js
@@ -1,7 +1,25 @@
 console.log('Starting notes.js');
 
 
-module.exports.addNote = () => {
-    console.log('Addnote');
-    return 'newNote';
+let addNote = (title, body) => {
+    console.log('Adding note', title, body);
+};
+
+let getAll = () => {
+    console.log('Getting all notes');
 }
+
+let readNote = (title) => {
+    console.log('Reading note', title);
+}
+
+let removeNote = (title) => {
+    console.log('Note has been removed.', title);
+}
+
+module.exports = {
+    addNote: addNote,
+    getAll: getAll,
+    readNote: readNote,
+    removeNote: removeNote
+};
