5a04b796672a52ec45b94913b110514389a25313
April Copes <antdevelopment1@gmail.com>
2018-11-03 21:48:00 -0400

    Uses yargs npm to parse new values passed to an object

--- note_app/app.js
+++ note_app/app.js
@@ -6,10 +6,16 @@ console.log('Starting App');
 // =======
 // Built in node modules 
 const fs = require('fs');
+
 // 3rd Party node module
 const _ = require('lodash'); 
+const yargs = require('yargs');
+
 // Local node modules
 const notes = require('./notes.js');
+
+// Uses yargs npm to parse passed values that get converted into an object
+const argv = yargs.argv;
 
 // Start with argv[2] & higher - argv[0] & argv[1] cannot be reassigned for new commands.
 // process.argv is part of the Node environment, not a 3rd party module.
@@ -19,7 +25,8 @@ let command = process.argv[2];
 console.log('Command: ', command);
 
 // Logs the updated array with our command
-console.log(process.argv);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
 
 // Checks if the command variable paased equals add, list, read, or, remove with error meesage otherwise.
 if (command === 'add') {
