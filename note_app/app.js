d5e1be9beca37ebd616d4f5ea4953c7b1a2e108a
April Copes <antdevelopment1@gmail.com>
2018-11-04 19:21:21 -0500

    refactors code to reuse logNote function to update user about note

--- note_app/app.js
+++ note_app/app.js
@@ -1,19 +1,16 @@
 // Starting Note App
 console.log('Starting App');
 
-// =======
-// MODULES
-// =======
+// =================
+//     MODULES
+// =================
 // Built in node modules 
 const fs = require('fs');
-
 // 3rd Party node module
 const _ = require('lodash'); 
 const yargs = require('yargs');
-
 // Local node modules
 const notes = require('./notes.js');
-
 // Uses yargs npm to parse passed values that get converted into an object
 const argv = yargs.argv;
 
@@ -29,20 +26,14 @@ console.log('Command: ', command);
 // console.log('Yargs', argv);
 // Checks if the command variable paased equals add, list, read, or, remove with error meesage otherwise.
 if (command === 'add') {
-
     // We save the function call of addNote called on a property of argv and save it to note.
     // This function is called on notes which is our parsed javascript object.
     let note = notes.addNote(argv.title, argv.body);
-
     // If our note is a valid object
     if (note) {
-
         // We log to the user the note has been created
         console.log('Note created.');
-        console.log('------');
-        console.log('Title: ' + note.title);
-        console.log('Body: ' + note.body);
-
+        notes.logNote(note);
     } else {
         // Otherwise if it's not a valid object becuase our title already exists we log to the user letting them know the title they have chosen already exsists
         console.log('Please choose a different title name. Title already exsists');
@@ -50,18 +41,21 @@ if (command === 'add') {
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
-    notes.readNote(argv.title);
+    let note = notes.readNote(argv.title);
+    if (note) {
+        console.log('Note found');
+        notes.logNote(note);
+    } else {
+        console.log('Note not found.')
+    }
 } else if (command === 'remove') {
-
     // We save the function call of removeNote called on a property of argv and save it to noteRemoved.
     // This function is called on notes which is our parsed javascript object.
     let noteRemoved = notes.removeNote(argv.title);
-
     // If our function call returns true then we will log to the user that a note was removed
     // Otherwise we want to log that the note was not found because has been removed
     let message = noteRemoved ? 'Note was removed' : 'Note was not found';
     console.log(message);
-
   //  If no valid command has been entered then we want to log command not recognized. 
 } else {
     console.log('Command not recognized')
