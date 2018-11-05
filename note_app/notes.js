d279d6d3c28f35b663cd64bd4bfe0485ce548d90
April Copes <antdevelopment1@gmail.com>
2018-11-04 22:06:05 -0500

    Removes excessive comments Refactor pending

--- note_app/notes.js
+++ note_app/notes.js
@@ -2,35 +2,40 @@
 const fs = require('fs');
 
 let fetchNotes = () => {
+
     try {
+
         let notesString = fs.readFileSync('notes-data.json');
+
         // We then parse the data into
         return JSON.parse(notesString);
+
     } catch (e) {
+
         return [];
+
     }
 };
 
 let saveNotes = (notes) => {
+
     fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+
 };
 
-// Function expression with an anonymous function that accepts two arguments title and body
+
 let addNote = (title, body) => {
    
     let notes = fetchNotes();
+
     // Saves an obejct inside our annoymous function
     let note = {
         title: title,
         body: body
     };
     
-    // Will store an array with all notes that already exsists inside the notes array that have the title of the note the user will try to create
-    // This line will return a bollean value to see if the note that got passed to our 
     let duplicateNotes = notes.filter((note) => note.title === title);
-
-    // If the length of our duplicateNotes === 0 meaning we have not yet duplicated the title name then we want to go ahead an execute our if statements code block
-    // Otherwise if we have duplicated the title name then we just won't push that data into our array
+    
     if (duplicateNotes.length === 0) {
         // Pushes the the strignified note into our notes array
         notes.push(note);
@@ -47,6 +52,7 @@ let getAll = () => {
 }
 
 let readNote = (title) => {
+
     // Fetches our javascript object
     let notes = fetchNotes();
 
@@ -55,6 +61,7 @@ let readNote = (title) => {
 
     // We return the value of the first and only index in our array
     return filteredNotes[0];
+
 }
 
 let removeNote = (title) => {
@@ -67,8 +74,7 @@ let removeNote = (title) => {
     // Only writes the data to our json file for values that were not passed by our user
     saveNotes(filteredNotes);
     
-    // Evaluates to see if our original object is the same length as our filtered object. This checks to see if their the same length
-    // because if they are not the same length this means a title has been deleted and we return that bollean value to our removeNote function
+   
     return notes.length !== filteredNotes.length;
 };
 
